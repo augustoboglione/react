fb95177f6a77ebe5a51b3f3c3770b6602d2f8211
Augusto Boglione Carrea <aguboglione@gmail.com>
2024-04-30 21:14:51 -0300

    fix hex

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -5,6 +5,7 @@ import Item from './Item.jsx'
 const ItemList = (props) => {
     const [map, setMap] = useState()
     const [classMap, setClassMap] = useState()
+    const containerWidth = document.querySelector('.container').offsetWidth
     
     useEffect(() => {
         setMap(props.products.map(product => <Item key={product.id} product={product}></Item>))
@@ -12,13 +13,20 @@ const ItemList = (props) => {
 
     useEffect(() => {
         const width = 346
-        const n = Math.floor(document.querySelector('.container').offsetWidth / width + 1 / 3)
-        console.log(n)
+        const n = Math.floor(containerWidth / width + 1 / 3)
+
         if (map) setClassMap(map.map((item, i) => {
-            console.log(n)
+
             if ((i + 1) % (2 * n - 1) == 0) return React.cloneElement(item, {className: ' end long'})
+            if ((i + 1) % 3 == 1 && n == 2) {
+                if (containerWidth > width * (n + 1 / 2)) return React.cloneElement(item, {className: ' only ext'})
+                return React.cloneElement(item, {className: ' only'})
+            }
             if ((i + 1) % (2 * n - 1) == 1) return React.cloneElement(item, {className: ' start short'})
-            if ((i + 1) % (2 * n - 1) == n - 1) return React.cloneElement(item, {className: ' end short'})
+            if ((i + 1) % (2 * n - 1) == n - 1) {
+                if (containerWidth > width * (n + 1 / 2)) return React.cloneElement(item, {className: ' end short ext'})
+                return React.cloneElement(item, {className: ' end short'})
+            }
             if ((i + 1) % (2 * n - 1) == n) return React.cloneElement(item, {className: ' start long'})
             return React.cloneElement(item, {className: ''})
         }))
