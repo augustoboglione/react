37f5e6876e3a4f534cf4ebf35652fcbe9c95c64e
Augusto Boglione Carrea <aguboglione@gmail.com>
2024-07-17 02:40:03 -0300

    remove tick

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,12 @@ import {CartProvider} from './context/CartContext.jsx'
 import Body from './Body.jsx'
 import Overscroll from './components/Overscroll.jsx'
 import './styles.css'
-import TickSvg from './svg/TickSvg.jsx'
-import tick from '/assets/tick.svg'
 
 const App = () => (
     <ThemeProvider>
         <AlertProvider>
             <CartProvider>
                 <Body/>
-                <TickSvg/>
-                <img src={tick} style={{width: 40}}/>
             </CartProvider>
         </AlertProvider>
         <Overscroll/>
